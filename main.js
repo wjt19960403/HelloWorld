5f6fe949365e989df8df7679b5cdaea8ef867b69
wjt19960403 <3098548617@qq.com>
2016-09-18 18:53:23 +0800

    编写了金字塔的代码，想得很美，但是有错误不知道在哪里。猜测是数组那里出现了问题

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function f() {
-    return function (s) { return s.length; };
+function f(x) {
+    var b; //用来存放每一行的内容的数组
+    for (var i = 0; i < x; i++) {
+        var a = x - i - 1; //某一行前面的空格数
+        b[i] = " ";
+        var num = (i + 1) * 2 - 1; //某一行*的数量
+        for (var j = 0; j < num; j++) {
+            b[i] = b[i] + "*";
+        }
+        console.log(b[i]);
+    }
 }
+f(3);
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-function f(): (x: string) => number {
-    return s => s.length;
-}
+
+function f(x:number):void {
+    var b:string[];//用来存放每一行的内容的数组
+    for(var i:number =0;i<x;i++){
+        var a:number = x-i-1;//某一行前面的空格数
+       b[i] = " "; 
+       var num:number = (i+1)*2-1;//某一行*的数量
+       for(var j:number =0;j<num;j++){
+           b[i] = b[i] + "*";
+       }
+       console.log(b[i]);
+    }
+
+    }
+
+f(3);
