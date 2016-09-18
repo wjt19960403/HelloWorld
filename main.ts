632b97170d14616bea4ce90d5c55d5be10b13222
wjt19960403 <3098548617@qq.com>
2016-09-18 20:43:59 +0800

    修改了代码的大部分，因为数组用不好所以干脆不用了，直接使用字符串反倒成功了。

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
 
 function f(x:number):void {
-    var b:string[];//用来存放每一行的内容的数组
+    //var b:string[]= new Array() ;//用来存放每一行的内容的数组
+    var floor:string;
+   // b[0] = " ";
     for(var i:number =0;i<x;i++){
-        var a:number = x-i-1;//某一行前面的空格数
-       b[i] = " "; 
-       var num:number = (i+1)*2-1;//某一行*的数量
-       for(var j:number =0;j<num;j++){
-           b[i] = b[i] + "*";
+       // var a:number = x-i-1;//某一行前面的空格数
+       floor = "";
+       var kong = x-i-1;
+       for(var j:number = 0;j<kong;j++){
+           floor += " ";
        }
-       console.log(b[i]);
+       var xing = i*2+1;
+       for(var k:number =0;k<xing;k++){
+           floor += "*";
+       }
+
+       //var num:number = (i+1)*2-1;//某一行*的数量
+       /* /for(var j:number =0;j<num;j++){
+           b[i] = b[i] + "*";
+       }*/
+       console.log(floor);
     }
 
     }
 
-f(3);
+f(5);
